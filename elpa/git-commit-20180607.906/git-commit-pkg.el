78701364bc7bd1be877e63e30b506a47fd88eda1
shae128 <slr772s@gmail.com>
2018-06-09 20:05:42 +0200

    adding MAGIT mode

---
+++ elpa/git-commit-20180607.906/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180607.906" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "3482d3dc0c944f30c9457bc27c8889c917457e64" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
